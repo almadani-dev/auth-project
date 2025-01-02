a0cc80c263063709b50b46b4ff577d822b848cb6
almadani-dev <mohammed@almadani.dev>
2025-01-02 13:48:39 +0200

    connect to db

--- actions/login.ts
+++ actions/login.ts
@@ -3,6 +3,7 @@
 import z from "zod";
 
 import { LoginSchema } from "@/schemas/index";
+import prisma from "@/lib/db";
 
 export const login = async (values: z.infer<typeof LoginSchema>) => {
   const validated = LoginSchema.safeParse(values);
