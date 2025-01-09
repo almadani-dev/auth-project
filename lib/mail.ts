7e5ac2cd0c1e27f4034dcb50141d2c6ffe44b641
almadani-dev <mohammed@almadani.dev>
2025-01-09 16:19:03 +0200

    Add Email

--- lib/mail.ts
+++ lib/mail.ts
@@ -5,7 +5,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 const domain = process.env.NEXT_PUBLIC_APP_URL;
 export const sendTwoFactorEmail = async (email: string, token: string) => {
   await resend.emails.send({
-    from: "onboarding@resend.dev",
+    from: "contact@almadani.dev",
     to: email,
     subject: "2FA Code",
     html: `<p>Your 2FA code: ${token} </p>`,
@@ -16,7 +16,7 @@ export const sendPasswordResetEmail = async (email: string, token: string) => {
   const resetLink = `${domain}/auth/new-password?token=${token}`;
 
   await resend.emails.send({
-    from: "onboarding@resend.dev",
+    from: "contact@almadani.dev",
     to: email,
     subject: "Reset Your Password",
     html: `<p>Click <a href=${resetLink}>here</a> to Reset Password</p>`,
